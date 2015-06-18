08d9e5a7a36191ee4616a643c4f61c858a33ac29
ladmusician.kim <ladmusician.kim@gmail.com>
2015-06-18 17:32:24 +0900

    AlarmCreate - ringtone 가져오기 구현

--- app/src/main/java/clom/goqual/goqualswitch/Alarm/ActivityCreateAlarm.java
+++ app/src/main/java/clom/goqual/goqualswitch/Alarm/ActivityCreateAlarm.java
@@ -1,10 +1,16 @@
 package clom.goqual.goqualswitch.Alarm;
 
 import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -18,19 +24,62 @@ public class ActivityCreateAlarm extends FragmentActivity {
     private static final String TAG = "ACTIVITY_CREATE_ALARM";
     private Context mContext;
 
+    private String mRingtoneUri = "";
+    private String mRingtoneTitle = "";
+    private boolean mIsValidateSound = false;
+
+    @InjectView(R.id.alarmcreate_txt_ringtone_title) TextView mTxtRingtoneTitle;
+
     @InjectView(R.id.alarmcreate_btn_cancel) Button mBtnCancel;
-    @OnClick({ R.id.alarmcreate_btn_cancel })
+    @OnClick({ R.id.alarmcreate_btn_cancel, R.id.alarmcreate_rl_ringtone })
     void onClickButton (View view) {
         switch(view.getId()) {
             case R.id.alarmcreate_rl_repeat:
                 break;
             case R.id.alarmcreate_rl_ringtone:
+                showRingtonePickerDialog();
                 break;
             case R.id.alarmcreate_btn_cancel:
                 finish();
                 break;
             default:
                 break;
+        }
+    }
+
+    // 벨소리 선택 dialog
+    private void showRingtonePickerDialog() {
+        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "SELECT RINGTONE:");
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE,RingtoneManager.TYPE_ALL);
+
+        startActivityForResult(intent, 0);
+    }
+
+    // startActivity 결과값 받아오기;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (resultCode) {
+            case -1:	// ringtone
+                dealRingtone(data);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void dealRingtone(Intent data) {
+        Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+        mRingtoneUri = uri.toString();
+        Log.e(TAG, "RINGTONE URL: " + mRingtoneUri);
+        if (uri != null) {
+            Ringtone ringtone = RingtoneManager.getRingtone(this, uri);
+            mRingtoneTitle = ringtone.getTitle(this);
+            Log.e(TAG, "RINGTONE TITLE: " + mRingtoneTitle);
+            mTxtRingtoneTitle.setText(mRingtoneTitle);
+            mIsValidateSound = true;
         }
     }
 
