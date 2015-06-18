9b18bd5894c3d77c63a1d58d9ef96ca0586ef02e
ladmusician.kim <ladmusician.kim@gmail.com>
2015-06-18 17:15:13 +0900

    AlarmCreate -> cancel 버튼 기능 구

--- app/src/main/java/clom/goqual/goqualswitch/Alarm/ActivityCreateAlarm.java
+++ app/src/main/java/clom/goqual/goqualswitch/Alarm/ActivityCreateAlarm.java
@@ -1,18 +1,43 @@
 package clom.goqual.goqualswitch.Alarm;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import clom.goqual.goqualswitch.R;
 
 /**
  * Created by admin on 2015. 6. 18..
  */
 public class ActivityCreateAlarm extends FragmentActivity {
+    private static final String TAG = "ACTIVITY_CREATE_ALARM";
+    private Context mContext;
+
+    @InjectView(R.id.alarmcreate_btn_cancel) Button mBtnCancel;
+    @OnClick({ R.id.alarmcreate_btn_cancel })
+    void onClickButton (View view) {
+        switch(view.getId()) {
+            case R.id.alarmcreate_rl_repeat:
+                break;
+            case R.id.alarmcreate_rl_ringtone:
+                break;
+            case R.id.alarmcreate_btn_cancel:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_create);
         ButterKnife.inject(this);
     }
     @Override
--- app/src/main/java/clom/goqual/goqualswitch/MainActivity.java
+++ app/src/main/java/clom/goqual/goqualswitch/MainActivity.java
@@ -1,12 +1,30 @@
 package clom.goqual.goqualswitch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import clom.goqual.goqualswitch.Alarm.ActivityCreateAlarm;
 
 
 public class MainActivity extends FragmentActivity {
+
+    @InjectView(R.id.btn_alarm) Button mBtnAlarm;
+    @OnClick({ R.id.btn_alarm })
+    void onClickButton (Button btn) {
+        switch(btn.getId()) {
+            case R.id.btn_alarm:
+                Intent alarmIntent = new Intent(getApplicationContext(), ActivityCreateAlarm.class);
+                startActivity(alarmIntent);
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
